409293d3cac980311c3e5792c4c506c88dcef6da
J.Valentine <jackvalentine@jackarchive.com>
2016-12-27 23:06:44 +0900

    databinding study
     - delete other and another component
     - add databinding component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<h1>Inline Template</h1>' +
-  '<fa-other></fa-other>' +
-  '<fa-another>' +
-    '<div>' +
-    '<h2>Hello!</h2>'+
-    '<p>World</p>' +
-    '</div>'+
-  '</fa-another>' +
-  '<fa-another>' +
-    '<p>Somthing else</p>' +
-  '</fa-another>',
-  styles: ['h1 { color: red;}']
+  '<fa-databinding></fa-databinding>'
+
 })
 export class AppComponent {
   title = 'I changed it!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
-import { AnotherComponent } from './other/another.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OtherComponent,
-    AnotherComponent
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
