0fa4ec5908e191edf6dee047ecde8561ccefab9e
J.Valentine <jackvalentine@jackarchive.com>
2017-01-20 22:38:46 +0900

    02-directive study mouseenter mouseleave
    - Add Hostlistener mouseenter event
    - Add Hostlistener mouseleave event

--- 02-directives/src/app/hightlight.directive.ts
+++ 02-directives/src/app/hightlight.directive.ts
@@ -1,11 +1,21 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[dirHightlight]'
 })
 export class HightlightDirective {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  };
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = 'white';
+  };
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  };
+  private backgroundColor = 'white';
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
     // this.elementRef.nativeElement.style.backgroundColor = 'green';
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
   }
 }
