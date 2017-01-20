8512e8175245046405a85da01722025bcf17d723
J.Valentine <jackvalentine@jackarchive.com>
2017-01-21 02:37:32 +0900

    02-directive study custom Structural
    - Add useless custom Structural

--- 02-directives/src/app/unless.directive.ts
+++ 02-directives/src/app/unless.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import {Directive, TemplateRef, ViewContainerRef, Input} from '@angular/core';
 
 @Directive({
   selector: '[dirUnless]'
 })
 export class UnlessDirective {
-
-  constructor() { }
+  @Input() set dirUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
+  constructor(private templateRef: TemplateRef<any>, private  vcRef: ViewContainerRef) { }
 
 }
