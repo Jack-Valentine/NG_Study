dc731f69557f7d2aba7671963a92d74d269574e9
J.Valentine <jackvalentine@jackarchive.com>
2017-01-26 00:35:46 +0900

    Study defendency injection
    - generate service
    - add service module

--- 03-service/src/app/app.module.ts
+++ 03-service/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServiceComponent } from "./service/service.component";
+import { CmpAComponent } from "./service/cmp-a.component";
+import { CmpBComponent } from "./service/cmp-b.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    CmpAComponent,
+    CmpBComponent
   ],
   imports: [
     BrowserModule,
