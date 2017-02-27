c29bb188fce7e6bb3a56fc5219f5aad248343369
J.Valentine <jackvalentine@jackarchive.com>
2017-02-27 20:39:38 +0900

    add form ngModel and name study form submit and form object

--- forms/src/app/template-driven/template-driven.component.ts
+++ forms/src/app/template-driven/template-driven.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'template-driven',
   templateUrl: 'template-driven.component.html'
 })
 export class TemplateDrivenComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
