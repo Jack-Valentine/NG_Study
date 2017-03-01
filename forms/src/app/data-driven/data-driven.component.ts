d81ff59a0e77c900dad7f67a8d1b128cf46ee7e1
J.Valentine <jackvalentine@jackarchive.com>
2017-03-01 23:33:39 +0900

    Study angular form reactiveform
    - add reactiveform module
    - change data-driven component

--- forms/src/app/data-driven/data-driven.component.ts
+++ forms/src/app/data-driven/data-driven.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl} from "@angular/forms";
 
 @Component({
     selector: 'data-driven',
     templateUrl: 'data-driven.component.html'
 })
 export class DataDrivenComponent {
-    
+    myForm: FormGroup;
+    constructor() {
+        this.myForm = new FormGroup({
+            'username': new FormControl(),
+            'email': new FormControl(),
+            'password': new FormControl()
+        });
+    }
 }
