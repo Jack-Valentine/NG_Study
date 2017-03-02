7ba536c8b7e75f7b9fdbed609add1d8ba38d7364
J.Valentine <jackvalentine@jackarchive.com>
2017-03-02 23:18:54 +0900

    Study form group
    - userData form group
    - move username and emain into userdata formGroup

--- forms/src/app/data-driven/data-driven.component.ts
+++ forms/src/app/data-driven/data-driven.component.ts
@@ -9,11 +9,13 @@ export class DataDrivenComponent {
     myForm: FormGroup;
     constructor() {
         this.myForm = new FormGroup({
-            'username': new FormControl('Max', Validators.required),
-            'email': new FormControl('', [
-                Validators.required,
-                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-            ]),
+            'userData': new FormGroup({
+                'username': new FormControl('Max', Validators.required),
+                'email': new FormControl('', [
+                    Validators.required,
+                    Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                ])
+            }),
             'password': new FormControl('', Validators.required)
         });
     }
