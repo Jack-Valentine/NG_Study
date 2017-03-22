45595788b807956aaba772e367212d0739d1b7cb
J.Valentine <jackvalentine@jackarchive.com>
2017-03-23 01:17:51 +0900

    Study ng2 pipes - study date pipe - study custom pipe - study pure pipe option - study async pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .pipes {
+      margin: 32px;
+      padding: 32px;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  myValue = 'lowercase';
+  myDate = new Date(2017, 3, 23);
+  values = ['Milk', 'Bread', 'Beans', 'Egg', 'Oil'];
+  asyncValue = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Data in here'), 2000);
+  });
 }
+
--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DoublePipe } from './double.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoublePipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
