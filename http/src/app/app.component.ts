3ee46b3222c963d1974f867bfa8a16688bc093ae
J.Valentine <jackvalentine@jackarchive.com>
2017-03-27 23:54:45 +0900

    Study get set data in firebase
    - add rxjs and set data in firebase
    - get firebase data and inject html

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -7,12 +7,33 @@ import {Response} from "@angular/http";
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  items: any[] = [];
+
   constructor(private httpService: HttpService) { }
+
   ngOnInit() {
     this.httpService.getDate()
       .subscribe(
-        (data: Response) => console.log(data)
+        (data: any) => console.log(data)
       );
-
+  }
+  onSubmit(username: string, email: string) {
+    this.httpService.sendData({username: username, email: email})
+      .subscribe(
+        data => console.log(data)
+      );
+  }
+  onGetData() {
+    this.httpService.getOwnData().subscribe(
+      data => {
+        const tmpArray = [];
+        for (let key in data) {
+          // console.log(data[key]);
+          tmpArray.push(data[key]);
+        }
+        this.items = tmpArray;
+        console.log(this.items);
+      }
+    );
   }
 }
--- http/src/app/http.service.ts
+++ http/src/app/http.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Http, Response, Headers} from '@angular/http';
+
+import 'rxjs/RX';
 
 @Injectable()
 export class HttpService {
   // private firebase url
   private json_url:string = '';
-  constructor(private http: Http) { }  getDate() {
-    return this.http.get(this.json_url);
+  private set_url:string = '';
+
+  constructor(private http: Http) { }
+
+  getDate() {
+    return this.http.get(this.json_url).map((response: Response) => response.json());
+  }
+
+  sendData(user: any) {
+    const body = JSON.stringify(user);
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(this.set_url, body, {headers: headers}).map((data: Response) => data.json());
+  }
+
+  getOwnData() {
+    return this.http.get(this.set_url).map((response: Response) => response.json());
   }
 }
