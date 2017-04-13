f9c3c82bd2ec1b3cd7b92278eb27e80d7dcd59ae
J.Valentine <jackvalentine@jackarchive.com>
2017-04-13 23:57:33 +0900

    Add shared and protected unprotected component

--- auth/src/app/app.module.ts
+++ auth/src/app/app.module.ts
@@ -4,10 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header.component';
+import { SigninComponent } from './unprotected/signin.component';
+import { SignupComponent } from './unprotected/signup.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SigninComponent,
+    SignupComponent,
+    ProtectedComponent,
+    FormBuilder,
+    FormGroup,
+    Validators,
+    FormControl
   ],
   imports: [
     BrowserModule,
