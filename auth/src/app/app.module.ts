0300007e1e5ca0bd28630601ba05993292392001
J.Valentine <jackvalentine@jackarchive.com>
2017-04-17 00:06:02 +0900

    upgrade angular version add module ReactiveFormsModule

--- auth/src/app/app.module.ts
+++ auth/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header.component';
@@ -20,6 +21,7 @@ import { ProtectedComponent } from './protected/protected.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
