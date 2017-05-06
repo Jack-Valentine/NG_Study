827785ebe97bf223325df1a4d72d972aea7c44c7
J.Valentine <jackvalentine@jackarchive.com>
2017-05-07 02:07:15 +0900

    change severs component change app component

--- ng4-complete-guide/src/app/servers/servers.component.ts
+++ ng4-complete-guide/src/app/servers/servers.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+      <app-server></app-server>
+      <app-server></app-server>
+  `,
+  // templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
