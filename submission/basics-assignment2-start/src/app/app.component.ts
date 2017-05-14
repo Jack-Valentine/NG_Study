cd46b366168b9085ce54922863874abfc8a449e1
J.Valentine <jackvalentine@jackarchive.com>
2017-05-15 00:44:07 +0900

    Add reset function in submission Add bootstrap style in submission Change submission style

--- submission/basics-assignment2-start/src/app/app.component.ts
+++ submission/basics-assignment2-start/src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username: string = 'test';
+  username: string = '';
 
-  onUpdateServerName(event: any) {
-    this.username = (<HTMLInputElement>event.target).value;
+  onReset(event: any) {
+    this.username = '';
   }
 }
