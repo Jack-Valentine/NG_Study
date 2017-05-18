89edc2eea0cb75bc948a08cc216c5152845708ec
J.Valentine <jackvalentine@jackarchive.com>
2017-05-18 23:59:35 +0900

    Study ng For

--- ng4-complete-guide/src/app/servers/servers.component.ts
+++ ng4-complete-guide/src/app/servers/servers.component.ts
@@ -15,6 +15,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2']
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +28,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
