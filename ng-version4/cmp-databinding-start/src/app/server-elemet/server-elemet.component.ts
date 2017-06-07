6227b05f727c6e4dbf82ed8a75a77ac442137b0c
J.Valentine <jackvalentine@jackarchive.com>
2017-06-07 22:44:28 +0900

    blueprint not visable bug fix

--- ng-version4/cmp-databinding-start/src/app/server-elemet/server-elemet.component.ts
+++ ng-version4/cmp-databinding-start/src/app/server-elemet/server-elemet.component.ts
@@ -1,12 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-server-elemet',
+  selector: 'app-server-element',
   templateUrl: './server-elemet.component.html',
   styleUrls: ['./server-elemet.component.css']
 })
 export class ServerElemetComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
