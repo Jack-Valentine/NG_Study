ee876d1177ec89c18ff19d1843b9d1df5390aae9
J.Valentine <jackvalentine@jackarchive.com>
2017-06-10 01:41:41 +0900

    Study angular view capsulation Study angular virtual dom Study Control @VIewChild Study how to use reuseable ng-content

--- ng-version4/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ ng-version4/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,26 +7,27 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();;
-  newServerName = '';
-  newServerContent = '';
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
--- ng-version4/cmp-databinding-start/src/app/server-elemet/server-elemet.component.ts
+++ ng-version4/cmp-databinding-start/src/app/server-elemet/server-elemet.component.ts
@@ -1,17 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-elemet.component.html',
-  styleUrls: ['./server-elemet.component.css']
+  styleUrls: ['./server-elemet.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElemetComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
-  ngOnInit() {
-    console.log(this.element);
-  }
+  ngOnInit() { }
 
 }
