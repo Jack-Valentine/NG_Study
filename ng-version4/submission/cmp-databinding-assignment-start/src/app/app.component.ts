0ec4e71336df8d5f24b4394f3b7038c0d0c9e8d9
J.Valentine <jackvalentine@jackarchive.com>
2017-06-25 20:39:48 +0900

    Add event emiter in game start add game start function

--- ng-version4/submission/cmp-databinding-assignment-start/src/app/app.component.ts
+++ ng-version4/submission/cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+ title = 'app works!';
+  onIntervalFired(firedNumber: number) {
+    console.log('test');
+    console.log(firedNumber);
+  }
 }
--- ng-version4/submission/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ ng-version4/submission/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval: any;
+  lastNumber = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+  }
+
+  onPauseGame() {
+  }
 }
