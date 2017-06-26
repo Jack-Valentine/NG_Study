3899121423a94198e8e75f569c93bd39e5437ab3
J.Valentine <jackvalentine@jackarchive.com>
2017-06-27 00:22:14 +0900

    Add odd and even color style Move even component

--- ng-version4/submission/cmp-databinding-assignment-start/src/app/even/even.component.ts
+++ ng-version4/submission/cmp-databinding-assignment-start/src/app/even/even.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-even',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./even.component.css']
 })
 export class EvenComponent implements OnInit {
-
+  @Input() number: number;
   constructor() { }
 
   ngOnInit() {
