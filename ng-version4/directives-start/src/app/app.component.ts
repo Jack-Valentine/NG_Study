b60640c717ee93782249f34b4328673ff448c5a5
J.Valentine <jackvalentine@jackarchive.com>
2017-07-08 00:57:53 +0900

    DIrective-start Project basic setting

--- ng-version4/directives-start/src/app/app.component.ts
+++ ng-version4/directives-start/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
