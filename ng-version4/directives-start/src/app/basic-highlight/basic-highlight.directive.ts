17c57dff745fda605a5bd6338551e68d07ae0f80
J.Valentine <jackvalentine@jackarchive.com>
2017-07-13 02:34:34 +0900

    Study custom element directive

--- ng-version4/directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ ng-version4/directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
-export class BasicHighlightDirective {
+export class BasicHighlightDirective implements OnInit{
+  constructor(private elementRef: ElementRef) {
+  }
 
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
 }
