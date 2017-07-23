e78975f49b88674f661b15a1e50be351b57d5a65
Jack <duerahan@gmail.com>
2017-07-23 14:25:20 +0900

    Change service-start npm package - fix bootstrap package

--- ng-version4/service-start/src/app/app.module.ts
+++ ng-version4/service-start/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    NewAccountComponent
   ],
   imports: [
     BrowserModule
