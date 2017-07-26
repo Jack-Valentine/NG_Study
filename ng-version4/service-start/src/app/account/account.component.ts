af82add9d5908f8d8948ad20db29cdb8024c3522
Jack <duerahan@gmail.com>
2017-07-26 20:53:31 +0900

    Spliting acoount component - add account.service - use logging service

--- ng-version4/service-start/src/app/account/account.component.ts
+++ ng-version4/service-start/src/app/account/account.component.ts
@@ -1,23 +1,24 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   ngOnInit() {
   }
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
-
 }
--- ng-version4/service-start/src/app/new-account/new-account.component.ts
+++ ng-version4/service-start/src/app/new-account/new-account.component.ts
@@ -4,12 +4,13 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) {}
 
   ngOnInit() {
   }
@@ -19,8 +20,9 @@ export class NewAccountComponent implements OnInit {
       name: accountName,
       status: accountStatus
     });
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
+    /*const service = new LoggingService();
+    service.logStatusChange(accountStatus);*/
     // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
